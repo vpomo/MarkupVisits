3ae4e75031570125f37d8def2df0e55fccdee3a1
vpomo <pomogalov@mail.ru>
2016-10-07 18:18:17 +0900

    + PROXY

--- src/main/java/com/vpomo/markupvisits/GoogleSuggest.java
+++ src/main/java/com/vpomo/markupvisits/GoogleSuggest.java
@@ -1,38 +1,58 @@
 package com.vpomo.markupvisits;
 
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.CapabilityType;
+import org.openqa.selenium.remote.DesiredCapabilities;
 
-import java.util.List;
 import java.util.concurrent.TimeUnit;
+
+import static java.lang.Thread.sleep;
 
 /**
  * Created by Pomogalov on 05.10.2016.
  */
 public class GoogleSuggest {
+    private static String baseUrl;
+
     public static void main(String[] args) throws Exception {
         // Драйвер Firefox поддерживает javascript
+        String proxyAddressPort;
+        String PROXY = "104.237.228.56:1080";
         System.setProperty("webdriver.chrome.driver", "D:/Java/WebDriver/chromedriver.exe");
-        WebDriver driver = new ChromeDriver();
-        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-        driver.manage().timeouts().pageLoadTimeout(10, TimeUnit.SECONDS);
 
-        // Открываем страничку Google
-        driver.get("http://www.google.ru");
+        org.openqa.selenium.Proxy proxy = new org.openqa.selenium.Proxy();
+        proxy.setHttpProxy(PROXY)
+                .setFtpProxy(PROXY)
+                .setSslProxy(PROXY)
+                .setSocksProxy(PROXY);
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability(CapabilityType.PROXY, proxy);
 
-        WebElement query = driver.findElement(By.name("q"));
-        // Вводим ключевое слово
-        query.sendKeys("гладиолус");
+        ChromeOptions option = new ChromeOptions();
+        option.addArguments("start-maximized");
+        capabilities.setCapability(ChromeOptions.CAPABILITY, option);
 
-        // Получаем список подсказок
-        List<WebElement> allSuggestions = driver.findElements(By.xpath("//td[@class='gssb_a gbqfsf']"));
-        // А затем выводим их в консоль
-        for (WebElement suggestion : allSuggestions) {
-            System.out.println(suggestion.getText());
-        }
-        if (driver != null) {
+        WebDriver driver = new ChromeDriver(capabilities);
+
+/*        proxyAddressPort = "104.237.228.56:1080"; 92.46.122.98:3128
+        System.out.print("proxy = " + proxyAddressPort);
+        ChromeOptions option = new ChromeOptions();
+        option.addArguments("start-maximized");
+        option.addArguments("--no-sandbox");
+        option.addArguments("--proxy-server=socks4://" + proxyAddressPort);
+*/
+        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+        driver.manage().timeouts().pageLoadTimeout(30, TimeUnit.SECONDS);
+
+        baseUrl = "http://speed-tester.info/check_ip.php";
+
+        try {
+            driver.get(baseUrl);
+            sleep(3000);
+            driver.quit();
+        } catch (Exception e) {
             driver.quit();
         }
     }
