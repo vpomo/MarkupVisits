8c0c621bf865143445deaf5f853a47fb36915a27
vpomo <pomogalov@gmail.com>
2016-10-11 00:18:50 +0900

    new FileListProxy

--- src/main/java/com/vpomo/markupvisits/os/makeJAR/MainMarkUp.java
+++ src/main/java/com/vpomo/markupvisits/os/makeJAR/MainMarkUp.java
@@ -1,7 +1,13 @@
 package com.vpomo.markupvisits.os.makeJAR;
 
+import com.vpomo.markupvisits.os.model.ListProxy;
 import com.vpomo.markupvisits.os.service.FileListProxy;
 import com.vpomo.markupvisits.os.service.WebService;
+import sun.util.calendar.BaseCalendar;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
 
 /**
  * Created by Pomogalov on 04.10.2016.
@@ -12,18 +18,22 @@ public class MainMarkUp {
         FileListProxy fileListProxy = new FileListProxy();
         WebService webService = new WebService();
         int result;
+        String currentProxy;
         String proxy1 = "5.9.117.40:3128";
         String proxy2 = "176.195.111.249:8080";
         String url1 = "https://whoer.net/ru";
         String url2 = "http://amit.ru";
 
-        //ArrayList<ListProxy> listProxy;
-        //listProxy = fileListProxy.readListProxy();
+        ArrayList<ListProxy> listProxy;
+        listProxy = fileListProxy.readListProxy();
         //System.out.println(listProxy.get(0).toString());
         //System.out.println(listProxy.get(1).toString());
 
-        result = webService.getBaseURL(proxy1, url1, "Узнать свой IP адрес");
+        currentProxy = listProxy.get(0).getAddressPort();
+
+        result = webService.getBaseURL(currentProxy, url1, "Узнать свой IP адрес");
         System.out.println("result = " + result);
+        fileListProxy.writeLog(result, currentProxy, url1);
 
         //result = clickLinkURL(proxy1, url2, "Сервисный центр", "Амурские Информационные Технологии");
         //System.out.println("result = " + result);
--- src/main/java/com/vpomo/markupvisits/os/service/FileListProxy.java
+++ src/main/java/com/vpomo/markupvisits/os/service/FileListProxy.java
@@ -3,20 +3,35 @@ package com.vpomo.markupvisits.os.service;
 import com.vpomo.markupvisits.os.model.ListProxy;
 
 import java.io.*;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 
 /**
  * Created by Zver on 09.10.2016.
  */
 public class FileListProxy {
-    public FileListProxy() {
+public static final String pathListProxy = "D:/Java/WebDriver/Proxy/listProxy.txt";
+public static final String pathBadProxy = "D:/Java/WebDriver/Proxy/badProxy.txt";
+public static final String pathLogs = "D:/Java/WebDriver/Logs/log.txt";
 
+
+    public void writeLog(int result, String proxy, String url){
+        String writedString;
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy.MM.dd 'at' HH:mm:ss z");
+
+        if (result == 1) {
+            writedString = dateFormat.format(new Date()) + " - " + proxy + " - " + url + " - " + "OK!";
+        } else {
+            writedString = dateFormat.format(new Date()) + " - " + proxy + " - " + url + " - " + "Error" + "(" + result + ")";
+        }
+        writeStringToFile(pathLogs, writedString);
     }
 
     public ArrayList<ListProxy> readListProxy() {
         ArrayList<ListProxy> myListProxy = new ArrayList<>();
         try {
-            File file = new File("D:/Java/WebDriver/listProxy.txt");
+            File file = new File(pathListProxy);
             //создаем объект FileReader для объекта File
             FileReader fileReader = new FileReader(file);
             //создаем BufferedReader с существующего FileReader для построчного считывания
@@ -35,6 +50,27 @@ public class FileListProxy {
             e.printStackTrace();
         }
         return myListProxy;
+    }
+
+    public void writeStringToFile(String pathToFile, String writingString) {
+        File file = new File(pathToFile);
+        FileWriter fileWriter = null;
+        BufferedWriter bufferedWriter = null;
+        String dataWithNewLine = writingString + System.getProperty("line.separator");
+        try{
+            fileWriter = new FileWriter(file, true);
+            bufferedWriter = new BufferedWriter(fileWriter);
+            bufferedWriter.write(dataWithNewLine);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }finally{
+            try {
+                bufferedWriter.close();
+                fileWriter.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
 
 }
